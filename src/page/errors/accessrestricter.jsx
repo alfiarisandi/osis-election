99e67fb87c78a3c1bc4947307da4e04b653ef213
alfiarisandi <alfyarisandy@gmail.com>
2023-01-13 21:28:08 +0700

    update href access restricted

--- src/page/errors/accessrestricter.jsx
+++ src/page/errors/accessrestricter.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './accessrestricted.css'
 import bgErrors from '../../img/errors-bg.png'
-import { Link } from 'react-router-dom'
 
 function Accessrestricted() {
   return (
